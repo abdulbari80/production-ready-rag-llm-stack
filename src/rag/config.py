73114f796d7197a2d9d6f7960ea6faa8563d6657
Abdul Bari <abdulbari80@gmail.com>
2025-11-16 12:59:20 +1100

    updated

--- src/rag/config.py
+++ src/rag/config.py
@@ -1,22 +1,18 @@
 from pydantic_settings import BaseSettings
-import os
-from dotenv import load_dotenv
-
-load_dotenv()  # Loads the .env file
 
 class Settings(BaseSettings):
-    # hf_token = os.getenv("HUGGINGFACE_API_TOKEN")
     data_dir: str = "data/text"
     chunk_size : int = 500
     chunk_overlap: int = 100
     faiss_index_dir: str = "vector_store/faiss_index"
     embedding_model_name: str = "all-MiniLM-L6-v2"
-    hfh_repo_id: str = "meta-llama/Llama-3.2-1B-Instruct"
+    hf_repo_id: str = "meta-llama/Llama-3.2-1B-Instruct"
     top_k: int = 3
     llm_temperature: float = 0.20
-    threshold: float = 0.40
+    threshold: float = 0.60
     relative_factor : float = 0.80
-    context_window_size: int = 2048
+    context_window_size: int = 1024
+    max_tokens = 600
 
 settings = Settings()
 
