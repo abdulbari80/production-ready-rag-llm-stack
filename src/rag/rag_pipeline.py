1092b79bfc49b14aef451945ba9d522533b6e696
Abdul Bari <abdulbari80@gmail.com>
2025-11-16 23:09:33 +1100

    Final modification for v1.0

--- src/rag/rag_pipeline.py
+++ src/rag/rag_pipeline.py
@@ -152,7 +152,6 @@ class RAGPipeline:
         sys_content = """
         You are a polite senior legal expert specializing in Australian privacy law.
         Follow this rules for generating answwer:
-        - You must begin the answer with: "Thanks for asking!"
         - DO NOT use disclaimer like "Based on the context..." or "As far as possible..."
         - Provide clear, concise and complete legal explanations.
         - If the user question is NOT relevant to privacy law, say so politely.
